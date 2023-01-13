f2150078199793dc5e8fa32e413430915a0b066f
veyselakbalik <veyselakbalik@ogr.iu.edu.tr>
2023-01-13 21:43:58 +0300

    Create tab bar items

--- RickAndMorty/Controllers/Core/RMLocationViewController.swift
+++ RickAndMorty/Controllers/Core/RMLocationViewController.swift
@@ -11,8 +11,9 @@ final class RMLocationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        title = "Locations"
+        
     }
     
 
